4cdd01e0b515bf44ed59d22d193dbec6810075bb
Koustubh Shrivatava <koustubh_shrivastava@external.mckinsey.com>
2014-07-03 17:52:50 +0530

    Line equality problem

--- lib/line.rb
+++ lib/line.rb
@@ -1,5 +1,33 @@
 class Line
-	def length
-		0
-	end
+
+  attr_reader :x1
+  attr_reader :y1
+  attr_reader :y2
+  attr_reader :x2
+  def initialize(x1, y1, x2, y2)
+    @x1=x1
+    @y1=y1
+    @x2=x2
+    @y2=y2
+  end
+
+  def equals?(x3, y3, x4, y4)
+    if (@x1==x3 && @y1==y3 && @x2==x4 && @y2==y4)
+      return true
+
+    elsif (@x1==x4 && @y1==y4 && @x2==x3 && @y2==y3)
+      return true
+    else
+      return false
+    end
+
+  end
+
+  def ==(line)
+
+    if (@x1 == line.x1 && @x2==line.x2 && @y1 == line.y1 && @y2==line.y2 )
+      return true
+    end
+
+  end
 end
--- spec/lib/line_spec.rb
+++ spec/lib/line_spec.rb
@@ -1,8 +1,21 @@
 require 'spec_helper'
 
 describe Line do
-  it "Length of line is 0" do
-    line = Line.new
-    expect (Line.length).to eq(0)
+  it "Should return boolean" do
+    line1=Line.new(1, 1, 2, 2)
+    line2=Line.new(1, 1, 2, 2)
+    expect(line1.==(line2)).to eq(true)
+  end
+end
+context Line do
+  it "Should return true for swaped coordinates" do
+    line=Line.new(1, 1, 2, 2)
+    expect(line.equals?(2, 2, 1, 1)).to eq(true)
+  end
+end
+context Line do
+  it "Should return false for different coordinates" do
+    line = Line.new(1, 2, 3, 4)
+    expect(line.equals?(5, 5, 6, 8)).to eq(false)
   end
 end
